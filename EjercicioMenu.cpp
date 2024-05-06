2f40a9dd4506508021c5791193ccee6e675c99ba
RodrigoHernandez06 <160176017+RodrigoHernandez06@users.noreply.github.com>
2024-05-06 12:40:08 -0600

    Add files via upload

---
+++ EjercicioMenu.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char x=0, plato=0;
+    
+    cout<<"MENU DEL DIA: \n"
+        <<"A - Pollo a la plancha \n"
+        <<"B - Carne asada \n"
+        <<"C - Sopa de gallina \n"
+        <<"D - Churrasco \n"
+        <<"E - Pescado frito\n";
+    cout<<"Ingrese la letra correspondiente a su plato: ";
+    cin>>plato;
+    
+    return 0;
+}
---
+++ EjercicioTabla.cpp
@@ -0,0 +1,17 @@
+//Tabla del 12
+
+#include <iostream>
+using namespace std;
+
+int main () 
+{
+    int i=1, r=0;
+    cout<<"Tabla del 12 \n";
+    while (i<=12)
+    {
+        r=i*12;
+        cout<<"12 x "<<i<<" = "<<r<<"\n";
+        i++;
+    }
+    return 0;
+}
